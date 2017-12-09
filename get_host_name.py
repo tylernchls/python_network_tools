e81a42885df709053dff44f2d4cd7620abab462f
tylernchls <tylernchls@gmail.com>
2017-12-09 11:25:30 -1000

    removed comments

--- get_host_name.py
+++ get_host_name.py
@@ -1,10 +1,9 @@
 #USES PYTHON 2.7
 
-#SCRIPT TO GET MACHINE NAME
 import socket
 def print_machine_info():
     host_name = socket.gethostname()
     print 'The hostname is: ', host_name
-    
+
 if __name__ == '__main__':
     print_machine_info()
--- port_scanner.py
+++ port_scanner.py
@@ -7,8 +7,6 @@ max_port = 81
 min_port = 70
 start_time = datetime.now()
 
-
-# Retreives ip address of remote target
 def get_remote_ip(target):
     print "-" * 60
     print "Please wait, determining ip address of: ", target
@@ -20,54 +18,47 @@ def get_remote_ip(target):
     print "-" * 60
     time.sleep(3)
 
-# Starts timer for scan.
 def start_scan_time():
     print "-" * 80
     print 'Starting port scan on: %s current time: %s' %  (target,start_time)
     print "-" * 80
     time.sleep(3)
 
-# Scans remote target based on host and port range by trying to make connection
+
 def scan_remote_host(target, port):
     try:
         # Creates new ipv4 socket, using tcp connection
         s = socket(AF_INET, SOCK_STREAM)
         response = s.connect_ex((target, port))
-        # If connect, print port then close socket
+
         if response == 0:
             print 'Port %d: Open' % port
         s.close()
-    # Allows program to keep running if error occurs
+
     except:
         pass
 
 def main():
-    ######## Code starts here on run.
+
     target = raw_input('Enter remote target: ')
 
     try:
-        # Calls function to get/display ip address of host
         get_remote_ip(target)
 
-        # Displays scan start time
         start_scan_time()
 
-    # Basic error handling
     except KeyboardInterrupt:
         print ' You pressed Ctrl+C. Shutting down program'
         sys.exit(1)
 
-    # Loops through port range
     for port in range(min_port, max_port):
         try:
-            # Calls function and passes host & port
             response = scan_remote_host(target, port)
 
             ##### Enables threading but no increase in speed
             # thread = threading.Thread(target=scan_remote_host, args=(target, int(port)))
             # thread.start()
 
-        # Allows program to keep running if error occurs
         except Exception, e:
             pass
 
