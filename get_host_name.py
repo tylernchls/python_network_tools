b2e773dfbcf0cb2c233d08e2f430bec8e18c1c52
tylernchls <tylernchls@gmail.com>
2017-12-09 15:55:43 -1000

    updated function name

--- get_host_name.py
+++ get_host_name.py
@@ -1,9 +1,9 @@
 #USES PYTHON 2.7
 
 import socket
-def print_machine_info():
+def print_host_name():
     host_name = socket.gethostname()
     print 'The hostname is: ', host_name
 
 if __name__ == '__main__':
-    print_machine_info()
+    print_machine_name()
